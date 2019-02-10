1b590c43bef9d2cad80ece2aa55643550255dbf9
169870MichelleRincon <47225452+169870MichelleRincon@users.noreply.github.com>
2019-02-09 23:46:14 -0700

    Add files via upload

---
+++ Ejercicio8_Pattern/Ejercicio8_Pattern.java
@@ -0,0 +1,14 @@
+//Nombre: Tanya Michelle Rincon Tarango
+//Dia: 09/02/19
+//Hora: 11:35pm - 11:43pm
+
+class Ejercicio8{
+	public static void main (String[] args){
+
+		System.out.println("    J     a    v     v  a");
+		System.out.println("    J    a a    v   v  a a");
+		System.out.println(" J  J   aaaaa    v v  aaaaa");
+		System.out.println("  JJ   a     a    v  a     a"); 
+		//imprime el patron de la palabra java
+	}
+}
