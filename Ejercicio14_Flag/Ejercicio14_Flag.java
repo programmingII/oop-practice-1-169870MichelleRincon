82e03be4ae5da5c198b49577bc13cdb57dd80e5a
169870MichelleRincon <47225452+169870MichelleRincon@users.noreply.github.com>
2019-02-11 21:27:50 -0700

    Add files via upload

---
+++ Ejercicio14_Flag/Ejercicio14_Flag.java
@@ -0,0 +1,23 @@
+//Nombre: Tanya Michelle Rincon Tarango
+//Dia: 11/02/19
+//Hora: 9:21pm - 9:26pm
+
+class Ejercicio14{
+	public static void main(String[] args){
+		System.out.println("* * * * * * =================================");
+		System.out.println(" * * * * *  =================================");
+		System.out.println("* * * * * * =================================");
+		System.out.println(" * * * * *  =================================");
+		System.out.println("* * * * * * =================================");
+		System.out.println(" * * * * *  =================================");
+		System.out.println("* * * * * * =================================");
+		System.out.println(" * * * * *  =================================");
+		System.out.println("* * * * * * =================================");
+		System.out.println("=============================================");
+		System.out.println("=============================================");
+		System.out.println("=============================================");
+		System.out.println("=============================================");
+		System.out.println("=============================================");
+		System.out.println("============================================="); //Se imprime la bandera
+	}
+}
