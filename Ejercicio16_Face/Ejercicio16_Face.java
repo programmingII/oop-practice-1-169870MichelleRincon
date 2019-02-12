58873d17bd7721c00e973758d50de86d9a64b7f2
169870MichelleRincon <47225452+169870MichelleRincon@users.noreply.github.com>
2019-02-12 12:34:37 -0700

    Add files via upload

---
+++ Ejercicio16_Face/Ejercicio16_Face.java
@@ -0,0 +1,13 @@
+//Nombre: Tanya Michelle Rincon Tarango
+//Dia: 07 / 02 / 19
+//Hora: 12:17pm - 12:32pm
+
+class Ejercicio16{
+	public static void main(String[] args){
+		System.out.println(" +\"\"\"\"\"+");
+		System.out.println("[| o o |]");
+		System.out.println(" |  ^  |");
+		System.out.println(" | \'-\' |");
+		System.out.println(" +\"\"\"\"\"+"); //Se manda a imprimir la figura
+	}
+}
